b76abe739cf6c1b92c714965be12bb24b4e7f400
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-08-05 19:45:05 +0000

    code in

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ var app = server();
 app.use(server.static(__dirname + '/public'));
 app.use('/bower_components', server.static(__dirname + '/bower_components'));
 app.use('/client', server.static(__dirname + '/client'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var path = require('path');
 var mongoose = require('mongoose');
 var Promise = require('es6-promise').Promise;
 var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectID;
  
-var io = require('socket.io')(app);
 
 // API Key: MMk5vnfEYNykynsDCYXy
 // https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json
 // http://socket.io/get-started/chat/
+
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
@@ -23,9 +29,7 @@ io.on('connection', function(socket){
   });
 }); 
 
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
  
 
 app.listen(port, function(){ 
