5d146ec0eface3a4369f828f788f449b13d39de4
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-08-07 00:15:13 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -51,15 +51,17 @@ myApp.controller('mainController', function($scope, $http, $window) {
     }
 
     $.each(names, function (i, name) {
+        //   https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?order=asc&column_index=4&collapse=daily&transformation=rdiff
 
-        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+        $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/'+name+'.json?order=asc&column_index=4&collapse=daily&transformation=rdiff',    function (data) {
 
             seriesOptions[i] = {
                 name: name,
-                data: data
+                data: data.dataset.data
             };
             
             console.log(data + " Data format.");
+            // https://www.highcharts.com/samples/data/jsonp.php?filename=msft-c.json&callback=jQuery31006323779385139796_1470527146017&_=1470527146018
 
             // As we're loading the data asynchronously, we don't know what order it will arrive. So
             // we keep a counter and create the chart when all the data is loaded.
