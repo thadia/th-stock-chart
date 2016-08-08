fdf3125e452230758ddc243f7ed620920b8bb965
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-08-08 23:19:15 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -53,8 +53,10 @@ myApp.controller('mainController', function($scope, $http, $window) {
     $.each(names, function (i, name) {
         //   https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?order=asc&column_index=4&collapse=daily&transformation=rdiff
        //https://www.quandl.com/api/v3/datasets/WIKI/'+name+'.json?order=asc&column_index=4&collapse=daily&transformation=none&start_date=2014-01-01&api_key=MMk5vnfEYNykynsDCYXy
-       
-        $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/'+name+'.json?order=asc&column_index=4&collapse=daily&transformation=none&api_key=MMk5vnfEYNykynsDCYXy&start_date=2014-01-01', function (data) {
+       var start_date=new Date();
+       start_date.setFullYear(start_date.getFullYear() - 1);
+    
+        $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/'+name+'.json?order=asc&column_index=4&collapse=daily&transformation=none&api_key=MMk5vnfEYNykynsDCYXy&start_date='+start_date.toISOString().slice(0, 10), function (data) {
             
             for(var j=0;j<data.dataset.data.length;j++){
                 data.dataset.data[j][0]=Date.parse(data.dataset.data[j][0]);
