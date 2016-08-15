770dd2b70427d187a0bf074d2130d46656c0f2f1
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-08-15 17:03:23 +0000

    commit

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ app.use('/client', server.static(__dirname + '/client'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-//var http = require('http').Server(app);
-//var io = require('socket.io')(http);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var connect = require('connect'),
-    socketio = require('socket.io');
-    
 var path = require('path');
 var mongoose = require('mongoose');
 var Promise = require('es6-promise').Promise;
@@ -27,22 +24,11 @@ var Schema = mongoose.Schema
 io.sockets.on('connection', function (socket) {
     console.log('A client is connected!');
 });
-
-io.sockets.on('add', function (socket) {
-    console.log('We are broadcasting a stock add.');
-});
+ 
 
 var data = [];
-Stock.find({}, '-_id',{},function(err, latest_data) {
-        if (err) return console.error(err);
-        else{
-          //console.log("latest_data " + latest_data);
-         data=latest_data;
-        }
-    });
 
 
-var io = socketio.listen(server);
 io.sockets.on('connection', function(socket) {
   socket.emit('change', data);
   socket.on('change', function(obj) {
@@ -89,6 +75,7 @@ app.get('/stocks/:mylist/all/names', function(req, res) {
         else{
           //console.log("latest_data " + latest_data);
           res.json(latest_data[0].stock_names);
+          data=latest_data[0].stock_names;
         }
     });
 
